488d2dfeb90b12010139ae0e540bd8b6400fa209
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-01-26 23:24:54 +0530

    login form

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./login.css"
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
+
+const Login = ({setLoginUser}) => {
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    email : "",
+    password:""
+  })
+
+
+  const handleChange = e => {
+    const { name , value} = e.target
+    setUser({
+      ...user,
+      [name]:value
+    })
+  }
+
+  const login = () => {
+    axios.post("http://localhost:9002/login" , user)
+    .then(res => {
+      alert(res.data.message)
+      setLoginUser(res.data.user)
+      navigate("/")
+    })
+       
+  }
+
   return (
-    <div>login</div>
+    <div className='login'>
+      {console.log(user)}
+        <h1> Login </h1>
+        <input type="text" name = "email"  value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
+        <input type="password" name = "password"  value={user.password} onChange={handleChange} placeholder="Enter your Email"></input>
+        <div className="button" onClick={login}>
+            Login
+        </div>
+        <div> New User ? Register Now 👇 </div>
+        <div className='button' onClick={() => navigate("/register")}> Register</div>
+    </div>
   )
 }
 
